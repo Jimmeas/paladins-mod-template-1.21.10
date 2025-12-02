ca686742f9bca219fd7f5d48030d6ad9be2d4277
Jimmeas <jimmeasxd@gmail.com>
2025-12-01 21:55:08 -0500

    Initial commit

---
+++ src/main/java/com/jimmeas/paladinsmod/PaladinsMod.java
@@ -0,0 +1,16 @@
+package com.jimmeas.paladinsmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PaladinsMod implements ModInitializer {
+	public static final String MOD_ID = "paladinsmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/jimmeas/paladinsmod/paladinsmodclient.java
@@ -0,0 +1,10 @@
+package com.jimmeas.paladinsmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class paladinsmodclient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
